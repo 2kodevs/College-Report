8de2dc9e37d9eca37df5f9c4e6d3c63c4764d05a
AntiD2ta <mtpotro41@gmail.com>
2020-03-12 10:12:14 -0400

    refs #3 - Get the career groups with more and less initial female inscription

--- data.py
+++ data.py
@@ -1,21 +1,30 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statistics
+import seaborn as sns
 import json
 from PIL import Image
 
-labels = []
-values = []
-with open("grupos.json", "r") as fd:
+
+data = []
+sns.set()
+
+###########Get the career groups with more and less initial female inscription in 2007-2019 period################################################
+with open("resources/db/grupos.json", "r") as fd:
     for k, v in json.load(fd).items():
-        labels.append(k)
-        values.append(v[1] * 100 / v[0])
+        if v[0] == 0:
+            data.append([0, k])
+            continue
+        data.append([v[1] * 100 / v[0], k])
 
-# print(labels)
-# print(values)
+df = pd.DataFrame(data, columns=["percentage", "group"]).sort_values(by="percentage")
+print(df)
 
-st.title("Mujeres en la universidad")
+df.plot(kind="bar", x="group", y="percentage", rot=25, title="Matrícula inicial universitaria por grupos de carreras en el período 2007-2019")
+plt.tight_layout()
+plt.show()
+###################################################################################################################################################
 
 text1 = '''
 Hay carreras universitarias en nuestro país cuya matrícula es predominantemente femenina. No es un fenómeno particular de una 
@@ -25,19 +34,15 @@ las carreras con mayor aglomeración de mujeres y qué tienen estas en común qu
 
 #Á É Í Ó Ú á é í ó ú ñ
 
-st.write(text1)
 
-def plot_bar_x():
-    index = np.arange(len(labels))
-    plt.bar(index, values)
-    plt.xlabel("Grupos de Carreras", fontsize=10)
-    plt.ylabel("Cantidad de Mujeres", fontsize=10)
-    plt.xticks(index, labels, fontsize=5, rotation=25)
-    plt.title("Cantidad de mujeres por grupo de carreras universitarias")
-    #plt.show()
-
-plot_bar_x()
-st.pyplot()
+# def plot_bar_x():
+#     index = np.arange(len(labels))
+#     plt.bar(index, values)
+#     plt.xlabel("Grupos de Carreras", fontsize=10)
+#     plt.ylabel("Cantidad de Mujeres", fontsize=10)
+#     plt.xticks(index, labels, fontsize=5, rotation=25)
+#     plt.title("Cantidad de mujeres por grupo de carreras universitarias")
+#     plt.show()
 
 #image = Image.open("Figure_2.jpeg")
 #st.image(image)#, width=2000)
